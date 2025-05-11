e992f13285ccd875f235008e4dbd5d5bd1deb4bf
Tabitha <tabithahsia@gmail.com>
2025-05-11 02:53:57 -0700

    Updated header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ const App = () => {
   return (
     <div className="App">
       <header>
-        <h1>Flixster</h1>
+        <h1>Tabitha&apos;s Flixster</h1>
       </header>
       <main>
         <MovieList />
       </main>
+      <footer className="site-footer">
+        <p>Tabitha&apos;s Flixster Fancy Footer</p>
+      </footer>
     </div>
   );
 };
