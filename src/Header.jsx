69889b7268439c3d8902a011584467ae0875172c
Tabitha <tabithahsia@gmail.com>
2025-05-11 21:21:39 -0700

    Added a banner

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,10 @@ export default function Header() {
   return (
     <header>
       <h1>Tabitha&apos;s Flixster</h1>
+      <section className="banner">
+        <h2>Welcome to Tabitha&apos;s Flixster</h2>
+        <p>Browser current movies or search for old ones!</p>
+      </section>
     </header>
   );
 }
